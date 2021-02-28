fdc7e80954b096c5be6397ac78797fd593001565
sppaul21 <72412173+sppaul21@users.noreply.github.com>
2021-02-27 20:27:09 -0700

    add name, edited addNumber() and reverseNumber()
    -name on line 2
    -addNumber() line 18, change =
          previousNumber = previousNumber + num
    -reverseNumber() added reverseNum in git class and modified code on line 51

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Sarah Paul
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum; 
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber = previousNumber + num;
+        return previousNumber;
     }
     
     /*****
@@ -45,6 +47,8 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
+        
+        reverseNum = newNumber;
         
         return newNumber;
     }
